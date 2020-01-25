56e554416fabc33c9e4971cfc364237ca5a71d1e
henriquesml <henrique_schmeller@hotmail.com>
2020-01-25 13:45:13 -0300

    adicionando .editorconfig

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const routes = new Router()
 
 routes.get('/', (req, res) => {
-    return res.json( { text: "hello" } )
+  return res.json( { text: "hello" } )
 })
 
 export default routes
