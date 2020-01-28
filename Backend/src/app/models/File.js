3b95606cf45d8e72c6c8bb56ed49da32d5e83194
henriquesml <henrique_schmeller@hotmail.com>
2020-01-28 20:27:55 -0300

    criando listagem de provedores de serviço

--- Backend/src/app/models/File.js
+++ Backend/src/app/models/File.js
@@ -6,6 +6,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get(){
+            return `http://192.168.15.9:3333/files/${this.path}`
+          }
+        }
       },
       {
         sequelize
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer'
 import multerConfig from './config/multer'
 
 import UserController from './app/controllers/UserController'
+import ProviderController from './app/controllers/ProviderController'
 import SessionController from './app/controllers/SessionController'
 import FileController from './app/controllers/FileController'
 import authMiddleware from './app/middlewares/auth'
@@ -12,11 +13,15 @@ const routes = new Router()
 const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store)
+
 routes.post('/sessions', SessionController.store)
 
 // Middleware Global
 routes.use(authMiddleware)
+
 routes.put('/users', UserController.update)
+
+routes.get('/providers', ProviderController.index)
 
 routes.post('/files', upload.single('file'), FileController.store)
 
