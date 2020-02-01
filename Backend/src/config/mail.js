c52916da39223d92e5d03ffe197ae1c755ae20b6
henriquesml <henrique_schmeller@hotmail.com>
2020-02-01 12:47:08 -0300

    criando envio de email ambiente de desenvolvimento

---
+++ Backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '3d1d4276cdaf28',
+    pass: '36448860a7b406'
+  },
+  default : {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+}
