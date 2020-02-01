a92a4163e97768a0bf55fa941eb4b96981cdceab
henriquesml <henrique_schmeller@hotmail.com>
2020-02-01 10:23:43 -0300

    adição do mongoose e correção de import

--- Backend/src/app/controllers/ScheduleController.js
+++ Backend/src/app/controllers/ScheduleController.js
@@ -1,4 +1,4 @@
-import { startOfDay, endOfDay, parseISO, startOfHour } from 'date-fns'
+import { startOfDay, endOfDay, parseISO } from 'date-fns'
 import { Op } from 'sequelize'
 import Appointment from '../models/Appointment'
 import User from '../models/User'
