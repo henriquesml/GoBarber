4c199529a4ea3ce83c82465ad1d73ba3d011cb61
henriquesml <henrique_schmeller@hotmail.com>
2020-02-01 12:03:57 -0300

    criando método para cancelar agendamento

--- Backend/src/app/controllers/AppointmentController.js
+++ Backend/src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup'
-import { startOfHour, parseISO, isBefore, format } from 'date-fns'
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns'
 import pt from 'date-fns/locale/pt'
 import Appointment from '../models/Appointment'
 import User from '../models/User'
@@ -9,6 +9,7 @@ import NotificationSchema from '../schemas/NotificationSchema'
 class AppointmentController {
 
   async index(req, res) {
+
     const { page = 1 } = req.query
 
     const appointment = await Appointment.findAll({
@@ -97,6 +98,26 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
       user: provider_id
     })
+
+    return res.json(appointment)
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id)
+
+    if (appointment.user_id != req.userId) {
+      return res.status(401).json({ error: "Não é possível excluir um agendamento de outro usuário." })
+    }
+
+    const dateWithSub = subHours(appointment.date, 2)
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({ error: "Só é possível cancelar agendamentos com mais de 2 horas de antecedência." })
+    }
+
+    appointment.canceled_at = new Date()
+
+    await appointment.save()
 
     return res.json(appointment)
   }
