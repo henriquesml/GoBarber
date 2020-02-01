2e99c06ab040a254d834566e594189778d9c9c0d
henriquesml <henrique_schmeller@hotmail.com>
2020-02-01 11:28:28 -0300

    criando notificações de agendamento via mongo db

--- Backend/src/app/controllers/AppointmentController.js
+++ Backend/src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup'
-import { startOfHour, parseISO, isBefore } from 'date-fns'
+import { startOfHour, parseISO, isBefore, format } from 'date-fns'
+import pt from 'date-fns/locale/pt'
 import Appointment from '../models/Appointment'
 import User from '../models/User'
 import File from '../models/File'
+import NotificationSchema from '../schemas/NotificationSchema'
 
 class AppointmentController {
 
@@ -77,6 +79,19 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date
+    })
+
+    // Cria a Notificação para o provedor
+   const user = await User.findByPk(req.userId)
+   const formattedDate = format(
+     hourStart,
+     "'dia' dd 'de' MMMM', às' H:mm'h'",
+     { locale: pt }
+     )
+
+    await NotificationSchema.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id
     })
 
     return res.json(appointment)
