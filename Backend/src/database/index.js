509b96068a1a64475f0f009f0893acece4d0c6a6
henriquesml <henrique_schmeller@hotmail.com>
2020-02-01 10:59:48 -0300

    criando conexão com o mongodb

--- Backend/src/database/index.js
+++ Backend/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize'
+import mongoose from 'mongoose'
 import databaseConfig from '../config/database'
 import User from '../app/models/User'
 import File from '../app/models/File'
@@ -9,12 +10,20 @@ const models = [User, File, Appointment]
 class Database {
   constructor() {
     this.init()
+    this.mongo()
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig)
     models.map(model => model.init(this.connection))
     .map(model => model.associate && model.associate(this.connection.models) )
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://192.168.99.100:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true }
+    )
   }
 }
 
