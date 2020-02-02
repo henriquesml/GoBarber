128ffc095c674eccabc749ed4c8d203c8547daf2
henriquesml <henrique_schmeller@hotmail.com>
2020-02-02 20:47:37 -0300

    ajustes na api

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import path from 'path'
+import cors from 'cors'
 import youch from 'youch'
 import * as Sentry from '@sentry/node'
 import routes from './routes'
@@ -21,6 +22,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors())
     this.server.use(express.json())
     this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
   }
--- Backend/src/app/controllers/SessionController.js
+++ Backend/src/app/controllers/SessionController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup'
 import jwt from 'jsonwebtoken'
 import authConfig from '../../config/auth'
 import User from '../models/User'
+import File from '../models/File'
 
 class SessionController {
   async store(req, res) {
@@ -16,7 +17,16 @@ class SessionController {
 
     const { email, password } = req.body
 
-    const user = await User.findOne({ where: { email } })
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url']
+        }
+      ]
+    })
 
     if ( !user ) {
       return res.status(401).json( { error: 'Usuário não encontrado. ' } )
@@ -26,13 +36,15 @@ class SessionController {
       return res.status(401).json( { error: 'Senha incorreta.' } )
     }
 
-    const { id, name } = user
+    const { id, name, avatar, provider } = user
 
     return res.json({
       user: {
         id,
         name,
-        email
+        email,
+        provider,
+        avatar
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn
