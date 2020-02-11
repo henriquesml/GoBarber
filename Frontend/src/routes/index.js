4642079d41ed8af422170a09a880436185b4cf8a
henriquesml <henrique_schmeller@hotmail.com>
2020-02-10 21:10:57 -0300

    adicionando token por default no header

--- Frontend/src/routes/index.js
+++ Frontend/src/routes/index.js
@@ -17,7 +17,7 @@ export default function Routes() {
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/profile" component={Profile} isPrivate />
 
-      <Route path="/"  isUndefined />
+      <Route path="/" isUndefined component={SignIn}/>
     </Switch>
   )
 }
