c44a100d1007ba1e44edc608eeea5382e5659126
henriquesml <henrique_schmeller@hotmail.com>
2020-02-16 19:09:49 -0300

    criação inicial da dashboard

--- Frontend/src/pages/Dashboard/index.js
+++ Frontend/src/pages/Dashboard/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
-// import { Container } from './styles';
+import api from '~/services/api'
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
   return (
-    <h1>Dashboard</h1>
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF"/>
+        </button>
+        <strong>16 de Fevereiro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF"/>
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+
+        <Time available>
+          <strong>10:00</strong>
+          <span>Em aberto</span>
+        </Time>
+
+        <Time>
+          <strong>10:00</strong>
+          <span>Henrique</span>
+        </Time>
+      </ul>
+    </Container>
   );
 }
